202b379e63acea12d92d16cf9e290f276d2001a9
vibhash1083 <vibhash1083.07@bitmesra.ac.in>
2019-10-17 21:59:00 +0530

    Initial commit

---
+++ src/main/java/com/booksworld/booksworld/BooksworldApplication.java
@@ -0,0 +1,13 @@
+package com.booksworld.booksworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksworldApplication.class, args);
+	}
+
+}
