27e5e21accf927a4f6c332312e126add9826ed01
Govardhana Dasari Krishnaiah <govardhana.k@SLS.ads.valuelabs.net>
2019-12-11 15:02:16 +0530

    Added user files

---
+++ src/main/java/com/booksworld/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.booksworld.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.booksworld.dao.User;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+	
+
+}
+
+
+
