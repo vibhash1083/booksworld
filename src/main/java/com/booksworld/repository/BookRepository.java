ac86403caf281ae73a1924faa0beba195b5bc24b
Vibhash Chandra <vibhash.chandra@valuelabs.com>
2019-12-30 19:54:52 +0530

    Formatted

--- src/main/java/com/booksworld/repository/BookRepository.java
+++ src/main/java/com/booksworld/repository/BookRepository.java
@@ -10,21 +10,17 @@ import com.booksworld.dao.Book.Status;
 
 import com.booksworld.dao.Book;
 
-
-
 @Repository
-public interface BookRepository extends JpaRepository<Book,Long>{
-	
-	@Query( value = "select * from BOOK  where BOOK.BOOK_NAME = :name",
-			 nativeQuery = true)
-	 List<Book> findBybook_name(@Param("name") String name) ;
-	@Query( value = "select * from BOOK  where BOOK.USER_ID = :userid and BOOK.BOOK_STATUS = :status",
-			 nativeQuery = true)
-	 List<Book> findByUserid_and_status(@Param("userid") long userid, @Param("userid") String status) ;
+public interface BookRepository extends JpaRepository<Book, Long> {
 
-	@Query( value = "select * from BOOK where BOOK.USER_ID = :userid",
-			 nativeQuery = true)
-	 List<Book> findByUserid(@Param("userid") long userid) ;
+	@Query(value = "select * from BOOK  where BOOK.BOOK_NAME = :name", nativeQuery = true)
+	List<Book> findBybook_name(@Param("name") String name);
+
+	@Query(value = "select * from BOOK  where BOOK.USER_ID = :userid and BOOK.BOOK_STATUS = :status", nativeQuery = true)
+	List<Book> findByUserid_and_status(@Param("userid") long userid, @Param("userid") String status);
+
+	@Query(value = "select * from BOOK where BOOK.USER_ID = :userid", nativeQuery = true)
+	List<Book> findByUserid(@Param("userid") long userid);
 
 //	@Query( value = "select * from transaction inner JOIN book ON transaction.book_id=book.id where transaction.status='booked' and user_id=user",nativeQuery = true)
 }
